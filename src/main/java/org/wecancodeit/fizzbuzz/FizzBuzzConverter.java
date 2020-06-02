2571da539b90632ef1996a1d6475597b55615cba
Noah Doughty <noahdoughty@gmail.com>
2020-06-02 03:55:54 -0400

    initial commit

---
+++ src/main/java/org/wecancodeit/fizzbuzz/FizzBuzzConverter.java
@@ -0,0 +1,7 @@
+package org.wecancodeit.fizzbuzz;
+
+public class FizzBuzzConverter {
+	public String convert(int i) {
+		return "1";
+	}
+}
--- src/test/java/org/wecancodeit/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/org/wecancodeit/fizzbuzz/FizzBuzzTest.java
@@ -2,9 +2,18 @@ package org.wecancodeit.fizzbuzz;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
-    @Test
-    public void shouldInstantiate(){
-        FizzBuzz underTest = new FizzBuzz();
-    }
+	@Test
+	public void shouldInstantiate() {
+		FizzBuzz underTest = new FizzBuzz();
+	
+}
+	
+	@Test
+	public void shouldConvertIntegerIntoString() {
+		FizzBuzzConverter fizzbuzz = new FizzBuzzConverter();
+		assertEquals("1", fizzbuzz.convert(1));
+	}
 }
